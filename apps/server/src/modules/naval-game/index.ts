c6ec4223ae7194e7ee0d86766b5bc19bce28b955
GabrielZilmar <gabrielzilmar@hotmail.com>
2024-10-31 16:07:20 -0300

    feat: add init adn attack to naval battle class

--- apps/server/src/modules/naval-game/index.ts
+++ apps/server/src/modules/naval-game/index.ts
@@ -1,8 +1,16 @@
 import { Player } from "~/modules/player";
 
+const BOARD_SIZE = 5;
+
 type ConstructorParams = {
   player1Id: number;
   player2Id: number;
+};
+
+type AttackParams = {
+  attackerId: number;
+  x: number;
+  y: number;
 };
 
 export class NavalGame {
@@ -11,12 +19,43 @@ export class NavalGame {
   private winner: Player | null;
 
   constructor({ player1Id, player2Id }: ConstructorParams) {
-    this.player1 = new Player({ id: player1Id });
-    this.player2 = new Player({ id: player2Id });
+    this.player1 = new Player({ id: player1Id, boardSize: BOARD_SIZE });
+    this.player2 = new Player({ id: player2Id, boardSize: BOARD_SIZE });
     this.winner = null;
   }
 
   get playerWinner(): Player | null {
     return this.winner;
+  }
+
+  public getPlayerById(id: number) {
+    return this.player1.id === id ? this.player1 : this.player2;
+  }
+
+  public getOpponent(playerId: number) {
+    return this.player1.id === playerId ? this.player2 : this.player1;
+  }
+
+  public attack({ attackerId, x, y }: AttackParams) {
+    const opponent = this.getOpponent(attackerId);
+    const attackSuccessfully = opponent.sufferAttack({ x, y });
+    if (opponent.hasNoShip) {
+      this.winner = this.getPlayerById(attackerId);
+    }
+    return attackSuccessfully;
+  }
+
+  public init() {
+    do {
+      const randomX = Math.floor(Math.random() * BOARD_SIZE);
+      const randomY = Math.floor(Math.random() * BOARD_SIZE);
+      this.player1.addShip({ x: randomX, y: randomY });
+    } while (!this.player1.allShipFilled);
+
+    do {
+      const randomX = Math.floor(Math.random() * BOARD_SIZE);
+      const randomY = Math.floor(Math.random() * BOARD_SIZE);
+      this.player2.addShip({ x: randomX, y: randomY });
+    } while (!this.player2.allShipFilled);
   }
 }
